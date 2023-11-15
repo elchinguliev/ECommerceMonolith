35e3d4517ccd60dd3ec4f57a57e24a523b51173b
Quliy_av90 <Quliy_av90@itstep.edu.az>
2023-11-15 11:46:39 +0400

    Add project files.

---
+++ ECommerce.WebUI/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Business.Abstract;
+using ECommerce.WebUI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartSessionService _cartSessionService;
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(ICartSessionService cartSessionService, IProductService productService, ICartService cartService)
+        {
+            _cartSessionService = cartSessionService;
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> AddToCart(int productId)
+        {
+            var productToBeAdded = await _productService.GetById(productId);
+            var cart = _cartSessionService.GetCart();
+            _cartService.AddToCart(cart, productToBeAdded);
+
+            _cartSessionService.SetCart(cart);
+
+            TempData.Add("message", String.Format("Your product, {0} was added successfully to cart", productToBeAdded.ProductName));
+
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
